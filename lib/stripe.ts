d8f7c2b5c914506fd0d1cb8e47c81c6608974d40
sumit <mazumdarsumit3@gmail.com>
2026-03-03 03:55:19 +0530

    chore: Update Stripe API version to 2022-08-01 and remove ts-expect-error.

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,7 +2,5 @@ import Stripe from 'stripe';
 import { env } from '@/schemas/env.schema';
 
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  // @ts-expect-error acacia is a valid api version
-  apiVersion: '2025-02-04.acacia',
+  apiVersion: '2022-08-01',
 });
- 
